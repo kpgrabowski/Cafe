3a0d5cf3a30a09a5fa5b8d6a32fa9b8d02401cc8
KGrabowski <Kamil.Grabowski@hemmersbach.com>
2017-03-02 15:53:56 +0100

    Finish ExtraStuffBuilder2

--- src/Model/ExtraStuffImpl.js
+++ src/Model/ExtraStuffImpl.js
@@ -18,32 +18,47 @@ class ExtraStuffImpl extends ExtraStuff {
 }
 
 
+    _checkRepresentation() {
+        assert(this._addSuger instanceof Boolean);
+        assert(this._addDoubleSuger instanceof Boolean);
+        assert(this._withoutSuger instanceof Boolean);
+        assert(this._addMilk instanceof Boolean);
+        assert(this._addDoubleMilk instanceof Boolean);
+        assert(this._withoutMilk instanceof Boolean);
+    }
+
     AddUpCost() {
     //TODO Implement method AddUpCost()
 
     }
 
     getAddSuger() {
+        this._checkRepresentation();
         return this._addSuger;
     }
 
     getAddDoubleSuger() {
+        this._checkRepresentation();
         return this._addDoubleSuger;
     }
 
     getWithoutSuger() {
+        this._checkRepresentation();
         return this._withoutSuger;
     }
 
     getAddMilk() {
+        this._checkRepresentation();
         return this._addMilk;
     }
 
     getAddDoubleMilk() {
+        this._checkRepresentation();
         return this._addDoubleMilk;
     }
 
     getWithoutMilk() {
+        this._checkRepresentation();
         return this._withoutMilk
     }
 

3c939fc58dba272c6d57ddefaf5f558ffc9d2144
KGrabowski <Kamil.Grabowski@hemmersbach.com>
2017-03-03 15:47:59 +0100

    15:47 03,02,2017

--- src/Model/ExtraStuffBuilder.js
+++ src/Model/ExtraStuffBuilder.js
@@ -1,18 +1,12 @@
 
 class ExtraStuffBuilder {
     constructor() {
-        this._addSuger = new Boolean(false);
-        this._addDoubleSuger = new Boolean(false);
-        this._withoutSuger = new Boolean(true);
-        this._addMilk = new Boolean(false);
-        this._addDoubleMilk = new Boolean(false);
-        this._withoutMilk = new Boolean(true);
+        this._allStuff = [];
     }
 
-    withAddSuger(addSuger) {
-        assert(addSuger instanceof Boolean);
-
-        this._addSuger = addSuger;
+    withAddSuger(flag) {
+        assert(addDoubleSuger instanceof Boolean);
+        this._allStuff += new SigleStuff(flag, new Number(1));
         return this;
     }
 
@@ -52,7 +46,7 @@ class ExtraStuffBuilder {
     }
 
     build() {
-        return new ExtraStuffImpl(this._addSuger, this._addDoubleSuger, this._withoutSuger, this._addMilk, this._addDoubleMilk, this._withoutMilk);
+        return new ExtraStuffImpl(this._allStuff);
     }
 
 }
--- src/Model/ExtraStuffImpl.js
+++ src/Model/ExtraStuffImpl.js
@@ -1,68 +1,59 @@
 class ExtraStuffImpl extends ExtraStuff {
-    constructor (addSuger, addDoubleSuger, withoutSuger, addMilk, addDoubleMilk, withoutMilk){
-        assert(addSuger instanceof Boolean);
-        assert(addDoubleSuger instanceof Boolean);
-        assert(withoutSuger instanceof Boolean);
-        assert(addMilk instanceof Boolean);
-        assert(addDoubleMilk instanceof Boolean);
-        assert(withoutMilk instanceof Boolean);
-
+    constructor (allStuff) {
         super();
-
-        this._addSuger = addSuger;
-        this._addDoubleSuger = addSuger;
-        this._withoutSuger = addSuger;
-        this._addMilk = addSuger;
-        this._addDoubleMilk = addSuger;
-        this._withoutMilk = addSuger;
-}
-
-
-    _checkRepresentation() {
-        assert(this._addSuger instanceof Boolean);
-        assert(this._addDoubleSuger instanceof Boolean);
-        assert(this._withoutSuger instanceof Boolean);
-        assert(this._addMilk instanceof Boolean);
-        assert(this._addDoubleMilk instanceof Boolean);
-        assert(this._withoutMilk instanceof Boolean);
+        this._allStuff = allStuff;
     }
-
     AddUpCost() {
     //TODO Implement method AddUpCost()
 
     }
 
     getAddSuger() {
-        this._checkRepresentation();
+
         return this._addSuger;
     }
 
-    getAddDoubleSuger() {
-        this._checkRepresentation();
+    getAddDoubleSuger(){
+
         return this._addDoubleSuger;
     }
 
     getWithoutSuger() {
-        this._checkRepresentation();
+
         return this._withoutSuger;
     }
 
     getAddMilk() {
-        this._checkRepresentation();
+
         return this._addMilk;
     }
 
     getAddDoubleMilk() {
-        this._checkRepresentation();
+
         return this._addDoubleMilk;
     }
 
     getWithoutMilk() {
-        this._checkRepresentation();
+
         return this._withoutMilk
     }
 
     static bilder() {
         return new ExtraStuffBuilder();
+    }
+}
+
+class SingleStuff{
+
+    _checkRepresentation() {
+        assert(this.flag instanceof Boolean);
+        assert(this.cost instanceof Number);
+    }
+
+    constructor(flag, cost){
+    assert(this.flag instanceof Boolean);
+    assert(this.cost instanceof Number);
+        this.flag = flag;
+        this.cost = cost;
     }
 }
